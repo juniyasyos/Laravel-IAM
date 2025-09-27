2cd538ff257f91123d717851ad0e03f7861e239c
juniyasyos <juniyasyos@gmail.com>
2025-09-27 23:32:46 +0700

    feat: Add 'active' column to users table and update UserFactory to set default value

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Config;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\PermissionRegistrar;
 use Spatie\Permission\Traits\HasRoles;
@@ -68,5 +70,19 @@ class User extends Authenticatable
     public function currentApplication(): ?Application
     {
         return $this->currentApplication;
+    }
+
+    /**
+     * Applications where the user holds roles within a team-scoped context.
+     */
+    public function applications(): BelongsToMany
+    {
+        $pivotTable = Config::get('permission.table_names.model_has_roles');
+        $teamForeignKey = Config::get('permission.column_names.team_foreign_key');
+        $modelMorphKey = Config::get('permission.column_names.model_morph_key');
+
+        return $this->belongsToMany(Application::class, $pivotTable, $modelMorphKey, $teamForeignKey)
+            ->wherePivot('model_type', self::class)
+            ->withPivot('role_id');
     }
 }
