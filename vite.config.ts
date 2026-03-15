bdbab79e9f3265450e4c74fa440522915285a125
juniyasyos <juniyasyos@gmail.com>
2026-03-16 04:03:43 +0700

    Remove unused JavaScript assets and update Vite configuration to disable app and SSR inputs

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.ts',
+                // 'resources/js/app.ts',
                 'resources/css/filament/panel/theme.css',
             ],
-            ssr: 'resources/js/ssr.ts',
+            // ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
         tailwindcss(),
