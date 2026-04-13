fdc1cdf3bffcc1f2cfabf30b9a236eb2aae0b971
juniyasyos <juniyasyos@gmail.com>
2026-04-13 07:38:14 +0700

    fix: update sync unit kerja config and add field status in user tabl

--- config/manage-unit-kerja.php
+++ config/manage-unit-kerja.php
@@ -15,9 +15,9 @@ return [
 
     'app_env' => env('MANAGE_UNIT_KERJA_APP_ENV', env('APP_ENV', 'production')),
 
-    'center_application' => env('MANAGE_UNIT_KERJA_CENTER_APPLICATION', false),
+    'center_application' => env('MANAGE_UNIT_KERJA_CENTER_APPLICATION', true),
 
-    'app_center_url' => env('MANAGE_UNIT_KERJA_APP_CENTER_URL', 'http://127.0.0.1:8088'),
+    'app_center_url' => env('MANAGE_UNIT_KERJA_APP_CENTER_URL', ''),
 
     'sync' => [
         'active' => env('MANAGE_UNIT_KERJA_SYNC_ACTIVE', true),
